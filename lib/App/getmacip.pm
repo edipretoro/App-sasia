3ee666b1a51e9a3ead066cba60e5d1fa0c23b8c3
Emmanuel Di Pretoro <edipretoro@gmail.com>
2016-02-03 22:49:03 +0100

    Adding the options and attributes of the tool

--- lib/App/getmacip.pm
+++ lib/App/getmacip.pm
@@ -10,6 +10,37 @@ use MooX::Options;
 use Nmap::Scanner;
 use YAML qw(LoadFile DumpFile Dump);
 use Template;
+has machines => (
+  is => 'rw',
+  isa => sub { die "$_[0] is not an hashref" unless ref($_[0]) eq 'HASH' },
+  default => sub { return {} },
+);
+
+option ip_range => (
+  is => 'ro',
+  format => 's',
+  short => 'i|ip',
+  required => 1,
+  default => sub { return '192.168.1.0/24' },
+  doc => 'The range of IP adresses to scan.'
+);
+
+option filter_filename => (
+  is => 'ro', 
+  format => 's',
+  required => 0,
+  short => 'f|filter',
+  doc => 'A YAML file containing information to filter and aggregate the scanning information.'
+);
+
+option template_filename => (
+  is => 'ro',
+  format => 's',
+  required => 0,
+  short => 't|template',
+  doc => 'A TT template to display the information.'
+);
+
 sub run {
 }
 
