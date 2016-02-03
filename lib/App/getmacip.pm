8cb96cc2f039c4a994e0218c2f98a8857f9b7e2a
Emmanuel Di Pretoro <edipretoro@gmail.com>
2016-02-03 22:47:48 +0100

    Adding the requirements

--- lib/App/getmacip.pm
+++ lib/App/getmacip.pm
@@ -2,10 +2,14 @@ package App::getmacip;
 
 use strict;
 use warnings;
+use Modern::Perl '2015';
 
 use Moo;
 use MooX::Options;
 
+use Nmap::Scanner;
+use YAML qw(LoadFile DumpFile Dump);
+use Template;
 sub run {
 }
 
